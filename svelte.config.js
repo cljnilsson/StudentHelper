6a583aaf8c5516f9b1b7c288c879094a7ab321b4
Lukas Nilsson <zkall@outlook.com>
2026-02-01 22:31:18 +0100

    First gh page working version

--- svelte.config.js
+++ svelte.config.js
@@ -2,21 +2,20 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html'
 		}),
+
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/StudentHelper',
+			base: dev ? '' : '/StudentHelper',
+			assets: dev ? '' : 'https://cljnilsson.github.io/StudentHelper'
 		}
 	}
 };
