78f32ccb4bd5d4b1bd7cd84b720fae8f7e63a594
Lukas Nilsson <zkall@outlook.com>
2026-02-01 22:11:55 +0100

    Wip2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	import './styles.scss';
 	import { type Snippet } from 'svelte';
+	import { asset } from '$app/paths';
 	let { children }: { children: Snippet<[]> } = $props();
+
+	const resolved = asset("/favicon.png");
+	const resolved1 = asset("/bootstrap/bootstrap.min.css");
+	const resolved2 = asset("/bootstrap/bootstrap-icons.min.css");
+	const resolved3 = asset("/bootstrap/bootstrap.bundle.min.js");
 </script>
 
 <svelte:head>
-	<link rel="icon" type="image/svg" href="/facicon.png" />
+	<link rel="icon" type="image/svg" href={resolved} />
 	<title>Study Helper</title>
 	<meta name="description" content="Questioner rotator" />
-	<link rel="stylesheet" href="/bootstrap/bootstrap.min.css" />
-	<link rel="stylesheet" href="/bootstrap/bootstrap-icons.min.css" />
-	<script src="/bootstrap/bootstrap.bundle.min.js" defer></script>
+	<link rel="stylesheet" href={resolved1} />
+	<link rel="stylesheet" href={resolved2} />
+	<script src={resolved3} defer></script>
 </svelte:head>
 
 <div class="container rounded">
