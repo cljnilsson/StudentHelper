5be84aa24607b79ed839770dc53d6241df3a0be1
Lukas Nilsson <zkall@outlook.com>
2026-02-01 22:05:09 +0100

    Updates

--- _app/immutable/chunks/COPDInaQ.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_t33xcr)==null?void 0:s.base)??"/StudentHelper";var t;const a=((t=globalThis.__sveltekit_t33xcr)==null?void 0:t.assets)??e??"";export{a,e as b};
